b0122d439ff5fd3b3aa81ca676e8c90b2b2b3554
Ross Barnowski <rossbar@berkeley.edu>
2020-10-24 22:14:20 -0700

    Switch to sphinx-book-theme.
    Provides more options for buttons in theme.
    
    Enable download and github interaction buttons.

--- site/conf.py
+++ site/conf.py
@@ -45,7 +45,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "github_url": "https://github.com/numpy/numpy-tutorials/",
+    "repository_url": "https://github.com/numpy/numpy-tutorials/",
+    "repository_branch": "master",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "path_to_docs": "site/",
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
